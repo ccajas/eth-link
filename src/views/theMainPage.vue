2ccb0b32b90dd6939861862ad3eec31670f64e34
ccajas <chris.cajas.m@gmail.com>
2019-03-29 20:25:50 -0500

    Update main page

--- src/views/theMainPage.vue
+++ src/views/theMainPage.vue
@@ -5,71 +5,76 @@
             <br/>
             -&#164;- work in progress -&#164;-
         </div>
-        <div class="row col-sm-5">
-            <div class="border-main">
-                <ul class="bg-main tabs border-main">
-                    <li class="pull-left">
-                        <vlink v-bind:href="'test'"><i class="glyphicon glyphicon-book" style="font-size: 0.7em"></i>
-                        Latest Blocks</vlink>
-                    </li>
-                    <li class="pull-left">
-                        <vlink v-bind:href="'test'"><i class="glyphicon glyphicon-briefcase" style="font-size: 0.7em"></i>
-                        Test</vlink>
-                    </li>
-                    <li class="pull-left">
-                        <vlink v-bind:href="'test'"><i class="glyphicon glyphicon-book" style="font-size: 0.7em"></i>
-                        Extra Tab</vlink>
-                    </li>
-                </ul>
+        <div class="row col-sm-10">
+            <div class="row tabs border-main">
+                <div class="border-main row col-sm-6">
+                    <ul class="bg-main tabs">
+                        <li class="pull-left">
+                            <vlink v-bind:href="'test'"><i class="glyphicon glyphicon-book" style="font-size: 0.7em"></i>
+                            Latest Blocks</vlink>
+                        </li>
+                        <li class="pull-left">
+                            <vlink v-bind:href="'test'"><i class="glyphicon glyphicon-briefcase" style="font-size: 0.7em"></i>
+                            Test</vlink>
+                        </li>
+                        <li class="pull-left">
+                            <vlink v-bind:href="'test'"><i class="glyphicon glyphicon-book" style="font-size: 0.7em"></i>
+                            Extra Tab</vlink>
+                        </li>
+                    </ul>
+                </div>
+                <div class="border-main row col-sm-6">
+                    <ul class="bg-main tabs">
+                        <li class="pull-left">
+                            <vlink v-bind:href="'test'"><i class="glyphicon glyphicon-briefcase" style="font-size: 0.7em"></i>
+                            Test</vlink>
+                        </li>
+                        <li class="pull-left">
+                            <vlink v-bind:href="'test'"><i class="glyphicon glyphicon-book" style="font-size: 0.7em"></i>
+                            Extra Tab</vlink>
+                        </li>
+                    </ul>
+                </div>
+                <div class="row col-sm-12 tabs">
+                    <div id="main-loader" class="bg-bright"></div>
+                </div>
             </div>
-            <div class="y-overflow-panel">
-                <ul>
-                    <li class="bg list-group-item"
-                        v-for = "block in blockList" 
-                        :key = "block.id">
-                        <blockLink :block="block.id">{{ block.id }}</blockLink>
-                            <span class="text-second pull-right">{{ block.time }}</span>
-                       {{ block.txLength }} transactions
-                            <span class="text-second pull-right">Mined by 
-                                <addrLink :addr="block.miner">{{ block.miner }}</addrLink></span>
-                        
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="row col-sm-5">
-            <div class="border-main">
-                <ul class="bg-main tabs border-main">
-                    <li class="pull-left">
-                        <vlink v-bind:href="'test'"><i class="glyphicon glyphicon-briefcase" style="font-size: 0.7em"></i>
-                        Test</vlink>
-                    </li>
-                    <li class="pull-left">
-                        <vlink v-bind:href="'test'"><i class="glyphicon glyphicon-book" style="font-size: 0.7em"></i>
-                        Extra Tab</vlink>
-                    </li>
-                </ul>
-            </div>
-            <div class="y-overflow-panel">
-                <table class="table table-dark">
-                    <thead>
-                        <tr class="border-main">
-                            <th style="width: 12%">Hash</th>
-                            <th style="width: 24%">From</th>
-                            <th style="width: 24%">To</th>
-                            <th style="width: 12%">Value</th>
-                            <th style="width: 12%">Gas Used</th>
-                            <th class="border-mid">Gas Price</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <txInfo
-                            v-for = "tx in txList" 
-                            :key = "tx.hash" 
-                            :tx = "tx">
-                        </txInfo>
-                    </tbody>
-                </table>
+            <div class="row">
+                <div class="row col-sm-6 y-overflow-panel">
+                    <ul>
+                        <li class="bg list-group-item"
+                            v-for = "block in blockList" 
+                            :key = "block.id">
+                            <blockLink :block="block.id">{{ block.id }}</blockLink>
+                                <span class="text-second pull-right">{{ block.time }}</span>
+                                {{ block.txLength }} transactions
+                                <span class="text-second pull-right">Mined by 
+                                    <addrLink :addr="block.miner">{{ block.miner }}</addrLink></span>
+                            
+                        </li>
+                    </ul>
+                </div>
+                <div class="row col-sm-6 y-overflow-panel">
+                    <table class="table table-dark">
+                        <thead>
+                            <tr class="border-main">
+                                <th style="width: 12%">Hash</th>
+                                <th style="width: 24%">From</th>
+                                <th style="width: 24%">To</th>
+                                <th style="width: 12%">Value</th>
+                                <th style="width: 12%">Gas Used</th>
+                                <th class="border-mid">Gas Price</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <txInfo
+                                v-for = "tx in txList" 
+                                :key = "tx.hash" 
+                                :tx = "tx">
+                            </txInfo>
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
         <!--<div class="col-sm-6 y-overflow-panel">
